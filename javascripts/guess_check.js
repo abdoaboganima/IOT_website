9a7851b1d2d79f22fb502f0c14248b53ad7573fe
Abdo <abdoaboganima@icloud.com>
2021-12-07 22:10:57 +0200

    Added The time delay, changed the layout of the game, added the navbar and corrected some typos

--- javascripts/guess_check.js
+++ javascripts/guess_check.js
@@ -1,21 +1,23 @@
 function test(){
-    alert("wellcome for guess check game you have 6 attempts");
+    alert("Welcome in guess game. You have 6 attempts to guess a number");
 
     let random_num = Math.floor((Math.random()*50)+1); 
-    alert(random_num);
+    alert(random_num);  //Showing the actual value
     let user_input=[],compare_num=0;
     let user_attempts=" your attempts are:",game_result="";
     let attempts=6;
+    var success;
 
     for (let index = 1; index <=attempts; index++) {
         
         user_input[index-1]=window.prompt("attempt "+index +" : ")
         if (user_input[index-1]>50|| user_input[index-1]<1) {
-            alert("please inter a numeber between 1-50");
+            alert("Please Enter a number between 1, 50");
             user_input[index-1]=window.prompt("attempt "+index +" : ")
         } else{
             if (user_input[index-1]==random_num) {
                 game_result="Right Answer!\n ";
+                success=true;
                 break;
             } else  {
                 compare_num=random_num - user_input[index-1];
@@ -34,6 +36,7 @@ function test(){
                 }
             if (index==attempts) {
                 game_result="Game Over ! \n" ;
+                success=false;
             }
 
             }
@@ -43,6 +46,14 @@ function test(){
     for (let i = 0; i < user_input.length; i++) {
         user_attempts += user_input[i] +"  ";
     };
-    alert(game_result + user_attempts);
+
+    /*For making a delay of 2 sec before showing the results*/
+    setTimeout(function() 
+    {
+        alert(game_result + user_attempts);
+        result=document.createElement("img");
+        result.setAttribute("src", success?"../images/youWin.jpg":"../images/youfail.jpeg")
+        document.getElementById("Game").append(result);
+    }, 2000);
     
 };
